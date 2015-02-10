2db1194b5cbdcb455a55bd725846eeca566dd0b3
ponmons <ponmons@naver.com>
2015-02-10 18:31:55 +0900

    commit 3

---
+++ Meeting/src/main/java/model/dao/memberDAO.java
@@ -0,0 +1,9 @@
+package model.dao;
+
+import java.util.List;
+
+import model.domain.memberDTO;
+
+public interface memberDAO {
+	List<memberDTO> memberSelect();
+}
---
+++ Meeting/src/main/java/model/domain/memberDTO.java
@@ -0,0 +1,68 @@
+package model.domain;
+
+public class memberDTO {
+	private int memno;
+	private String memname;
+	private String mempw;
+	private String memid;
+	private String account;
+	private int phonenumber;
+	public memberDTO() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public memberDTO(int memno, String memname, String mempw, String memid,
+			String account, int phonenumber) {
+		super();
+		this.memno = memno;
+		this.memname = memname;
+		this.mempw = mempw;
+		this.memid = memid;
+		this.account = account;
+		this.phonenumber = phonenumber;
+	}
+	public int getMemno() {
+		return memno;
+	}
+	public void setMemno(int memno) {
+		this.memno = memno;
+	}
+	public String getMemname() {
+		return memname;
+	}
+	public void setMemname(String memname) {
+		this.memname = memname;
+	}
+	public String getMempw() {
+		return mempw;
+	}
+	public void setMempw(String mempw) {
+		this.mempw = mempw;
+	}
+	public String getMemid() {
+		return memid;
+	}
+	public void setMemid(String memid) {
+		this.memid = memid;
+	}
+	public String getAccount() {
+		return account;
+	}
+	public void setAccount(String account) {
+		this.account = account;
+	}
+	public int getPhonenumber() {
+		return phonenumber;
+	}
+	public void setPhonenumber(int phonenumber) {
+		this.phonenumber = phonenumber;
+	}
+	@Override
+	public String toString() {
+		return "memberDTO [memno=" + memno + ", memname=" + memname
+				+ ", mempw=" + mempw + ", memid=" + memid + ", account="
+				+ account + ", phonenumber=" + phonenumber + "]";
+	}
+	
+	
+}
